47158b1c7a08fad3d86331e31e10e77690ae3065
Unknown <mgijon94@gmail.com>
2019-06-29 19:18:12 +0200

    updated

--- WER/principal.py
+++ WER/principal.py
@@ -1,36 +1,33 @@
-from nltk.corpus import wordnet as wn
+# Python basic package utilities
 import os
-
 import pickle as plk
 import random
 import pandas as pd
 from tempfile import TemporaryFile
-
+import heapq
+import logging
+import functools
+import time
+# NLTK package
+from nltk.corpus import wordnet as wn
+# Scipy package
 import scipy.spatial.distance
 from scipy.stats import entropy
-
+from scipy import stats
+# Numpy package
 import numpy as np
 from numpy.linalg import norm
-
+# Gensim package
 import gensim
 import gensim.models as gm
 from gensim.test.utils import datapath, get_tmpfile
 from gensim.models import KeyedVectors
 from gensim.scripts.glove2word2vec import glove2word2vec
-
+# Sklearn package
 import sklearn.metrics
 from sklearn.metrics import pairwise_distances
 from sklearn.metrics.pairwise import pairwise_kernels
 from sklearn.preprocessing import normalize
-
-import heapq
-import logging
-
-import functools
-import time
-from scipy import stats
-
-
 
 class WER(object):
     '''
